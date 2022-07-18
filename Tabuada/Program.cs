c7189503766b16c89b924f1b158dd89c359fae9b
Lucas Fernandes <lucas-flana@hotmail.com>
2022-07-18 10:35:37 -0300

    tabuada c#

---
+++ Tabuada/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Tabuada
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        { 
+            for (int x = 2; x <= 2; x++)
+            {
+                for(int y = 0; y <=10; y++)
+                {
+                    Console.WriteLine(x + "x" + y + "=" + x * y);
+                }
+            }
+        }
+    }
+}
